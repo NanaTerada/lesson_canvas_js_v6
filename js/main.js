b1f5a9f4abf40b2e0e849fe164d8e9be1c8c63b5
n2 <7worki.nprogress@gmail.com>
2020-05-18 16:35:44 +0900

    #03 四角を描画してみよう

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@
 
 {
     function draw() {
-
+        const canvas = document.querySelector('canvas'); //canvas要素を取得して定数にいれる
+        if (typeof canvas.getContext === 'undefined') { //ブラウザがcanvasをサポートしていなかったら処理を終了
+            return;
+        }
+        const ctx = canvas.getContext('2d'); //ctx =　コンテクスト 絵筆のようなもの。。。？
+        //ctx.fillRect(x,y,width,height); 
+        //ctx.fillRect(50,50,50,50); 塗りつぶされた四角
+        ctx.strokeRect(50,50,50,50);　//わくだけの四角
     }
+
     draw();
 }
