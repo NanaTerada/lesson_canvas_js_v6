0ba9e027fd5e6ccd6398678154f119b1ae295394
n2 <7worki.nprogress@gmail.com>
2020-05-19 18:32:11 +0900

    #19 アニメーションさせてみよう

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 {
+    let t  = 0;
+
     function draw() {
         const canvas = document.querySelector('canvas'); //canvas要素を取得して定数にいれる
         if (typeof canvas.getContext === 'undefined') { //ブラウザがcanvasをサポートしていなかったら処理を終了
@@ -8,44 +10,24 @@
         }
         const ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = 'pink';
-        ctx.fillRect(0,0,200,200);
+        ctx.clearRect(0,0,canvas.width,canvas.height);
 
+        
         ctx.beginPath();
         ctx.ellipse(100,100,40,30,0,0,2 * Math.PI);
         ctx.fillStyle = 'black';
         ctx.fill();
 
         ctx.beginPath();
-        ctx.ellipse(80,100,8,8,0,0,2 * Math.PI);
-        ctx.ellipse(120,100,8,8,0,0,2 * Math.PI);
+        ctx.ellipse(80 + Math.sin(t / 30),100,8,8,0,0,2 * Math.PI);
+        ctx.ellipse(120 + Math.sin(t / 30),100,8,8,0,0,2 * Math.PI);
         ctx.fillStyle = 'skyblue';
         ctx.fill();
 
-        ctx.save();
+        t++;
+        setTimeout(draw,10);
 
-        ctx.scale(0.5,0.5); //それ以降半分の位置に描画される
-        ctx.translate(400,0);
-        ctx.rotate(45 / 180 *Math.PI);
-
-        ctx.fillStyle = 'olive';
-        ctx.fillRect(0,0,200,200);
-
-        ctx.beginPath();
-        ctx.ellipse(100,100,40,30,0,0,2 * Math.PI);
-        ctx.fillStyle = 'black';
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.ellipse(80,100,8,8,0,0,2 * Math.PI);
-        ctx.ellipse(120,100,8,8,0,0,2 * Math.PI);
-        ctx.fillStyle = 'skyblue';
-        ctx.fill();
-
-        ctx.restore();
-        ctx.fillStyle = 'black';
-
-        ctx.fillRect(80,120,40,40);
+        
     }
 
     draw();
